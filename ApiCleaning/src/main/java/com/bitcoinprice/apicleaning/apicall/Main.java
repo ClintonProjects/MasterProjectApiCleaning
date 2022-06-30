6b8a0c1267bff76b6604b3a5c5cf760a11c5e5fd
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2022-06-30 03:19:47 +0100

    Wow

--- ApiCleaning/src/main/java/com/bitcoinprice/apicleaning/apicall/Main.java
+++ ApiCleaning/src/main/java/com/bitcoinprice/apicleaning/apicall/Main.java
@@ -1,6 +1,8 @@
 package com.bitcoinprice.apicleaning.apicall;
-//lk
+
 public class Main {
-	 public static void main(String[] args) throws Exception {
-	 }
+	public static void main(String[] args) throws Exception {
+		System.out.println("WOW");
+	}
+	
 }
